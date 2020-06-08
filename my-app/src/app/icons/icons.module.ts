b3939a7336a5b0c165f20bb2720243d0a7cbb47d
tbotquin <thomas.botquin@gmail.com>
2020-06-08 16:27:35 +0200

    Ajout des composants icons et création de la base d'ui

--- my-app/src/app/icons/icons.module.ts
+++ my-app/src/app/icons/icons.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { IconEditComponent } from './components/icon-edit/icon-edit.component';
+import { IconTrashComponent } from './components/icon-trash/icon-trash.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconNavComponent } from './components/icon-nav/icon-nav.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [IconEditComponent, IconTrashComponent, IconNavComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
+  ],
+  exports: [
+    IconTrashComponent, IconNavComponent, IconEditComponent
   ]
 })
 export class IconsModule { }
