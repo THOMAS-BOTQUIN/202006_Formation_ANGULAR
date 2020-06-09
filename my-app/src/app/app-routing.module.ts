5df1fc37b8ae4c976d420b3ca74902967561a568
tbotquin <thomas.botquin@gmail.com>
2020-06-09 11:55:41 +0200

    Ajout des logs pour le routeur

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { PageLoginComponent } from './login/pages/page-login/page-login.component';
+import { Routes, RouterModule, PreloadAllModules, Router } from '@angular/router';
 
 
 const routes: Routes = [
@@ -20,7 +19,17 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: false,
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+    // Diagnostic only: inspect router configuration
+    constructor(router: Router) {
+      // Use a custom replacer to display function names in the route configs
+      const replacer = (key, value) => (typeof value === 'function') ? value.name : value;
+      console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+    }
+}
